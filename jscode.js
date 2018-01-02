8b1faadc38cfe50c423c2798f5d2718b6c4edf5e
jiankesheji <34343326+jiankesheji@users.noreply.github.com>
2018-01-02 17:52:30 +0800

    Update and rename jscode to jscode.js

---
+++ jscode.js
@@ -0,0 +1,14 @@
+//颠倒字符串
+newstr= str.split("").reverse().join("");
+//首字母大写
+function titleCase(str) {
+  var array = str.split(" ");
+  for (var i = 0; i < array.length; i++){
+    array[i] = array[i][0].toUpperCase() + array[i].substring(1, array[i].length).toLowerCase();
+
+    //array[i][0] = array[i][0] + 'A' - 'a';
+  }
+  var string = array.join(" ");
+
+  return string;
+}
