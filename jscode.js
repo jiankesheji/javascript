4032fe042ad633ca86a17cdefff47e1f74c5bd64
jiankesheji <34343326+jiankesheji@users.noreply.github.com>
2018-01-02 17:53:38 +0800

    Update jscode.js

--- jscode.js
+++ jscode.js
@@ -5,8 +5,6 @@ function titleCase(str) {
   var array = str.split(" ");
   for (var i = 0; i < array.length; i++){
     array[i] = array[i][0].toUpperCase() + array[i].substring(1, array[i].length).toLowerCase();
-
-    //array[i][0] = array[i][0] + 'A' - 'a';
   }
   var string = array.join(" ");
 
