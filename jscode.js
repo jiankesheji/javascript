c065157cc40446e5cde9ae46059ac5d96a7d7ed9
jiankesheji <34343326+jiankesheji@users.noreply.github.com>
2018-01-03 15:27:04 +0800

    Update jscode.js

--- jscode.js
+++ jscode.js
@@ -1,3 +1,4 @@
+//18.1.2
 //颠倒字符串
 newstr= str.split("").reverse().join("");
 //首字母大写
@@ -12,4 +13,12 @@ function titleCase(str) {
 //检查字符串结尾是否和条件一致
 function confirmEnding(str, target) {
   return (str.substr(-target.length)==target);
+}
+//18.1.3
+//实现一个摧毁(destroyer)函数，第一个参数是待摧毁的数组，其余的参数是待摧毁的值
+// [].slice.call(arguments) 转arguments为数组；reduce(）累加；filter（）筛选。
+function destroyer(arr) {
+ return [].slice.call(arguments).reduce(function (prev, next) {  
+        return prev.filter( e => e !== next);  
+    });  
 }
