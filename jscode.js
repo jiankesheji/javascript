8d8c63a950bb710d3c804f1e9ba30f4dd807ebba
jiankesheji <34343326+jiankesheji@users.noreply.github.com>
2018-01-04 14:59:55 +0800

    Update jscode.js

--- jscode.js
+++ jscode.js
@@ -1,4 +1,3 @@
-//18.1.2
 //颠倒字符串
 newstr= str.split("").reverse().join("");
 //首字母大写
@@ -14,11 +13,25 @@ function titleCase(str) {
 function confirmEnding(str, target) {
   return (str.substr(-target.length)==target);
 }
-//18.1.3
 //实现一个摧毁(destroyer)函数，第一个参数是待摧毁的数组，其余的参数是待摧毁的值
 // [].slice.call(arguments) 转arguments为数组；reduce(）累加；filter（）筛选。
 function destroyer(arr) {
  return [].slice.call(arguments).reduce(function (prev, next) {  
         return prev.filter( e => e !== next);  
     });  
+}
+//将给定的数字转换成罗马数字。
+//array.forEach(function(Value, index, array);  forEach() 方法对数组的每个元素执行一次提供的函数;
+//Value(当前值)index(索引)array(forEach方法正在操作的数组.) 
+function convert(num) {
+ var nums = [1000,900,500,400,100,90,50,40,10,9,5,4,1];  
+  var romans =["m","cm","d","cd","c","xc","l","xl","x","ix","v","iv","i"];  
+  var str = '';  
+  nums.forEach(function(item,index,array){  
+    while(num >= item){  
+      str += romans[index];  
+      num -= item;  
+    }  
+  });  
+ return str.toUpperCase();  
 }
